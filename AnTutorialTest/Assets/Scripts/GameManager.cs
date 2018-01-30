8a99321661d7db8332f25f667c066c57ddeabfc0
StevePro7 <steveprozune@hotmail.com>
2018-01-30 19:31:41 +0000

    Add game manager

---
+++ AnTutorialTest/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace SteveProStudios.AnTutorialTest
+{
+	public class GameManager : MonoBehaviour
+	{
+
+		/// <summary>
+		/// Called when the local player left the room. We need to load the launcher scene.
+		/// </summary>
+		public void OnLeftRoom()
+		{
+			SceneManager.LoadScene(0);
+		}
+
+		public void LeaveRoom()
+		{
+			PhotonNetwork.LeaveRoom();
+		}
+	}
+}
