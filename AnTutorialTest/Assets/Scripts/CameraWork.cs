e0de99120f22c87797da88e6b13378c80800c59e
StevePro7 <steveprozune@hotmail.com>
2018-01-31 16:55:28 +0000

    Update scene

---
+++ AnTutorialTest/Assets/Scripts/CameraWork.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using UnityEngine;
+
+/// <summary>
+/// Camera work. Follow a target
+/// </summary>
+public class CameraWork : MonoBehaviour
+{
+	[Tooltip("The distance in the local x-z plane to the target")]
+	public float distance = 7.0f;
+
+	[Tooltip("The height we want the camera to be above the target")]
+	public float height = 3.0f;
+
+	[Tooltip("The Smooth time lag for the height of the camera.")]
+	public float heightSmoothLag = 0.3f;
+
+	[Tooltip("Allow the camera to be offseted vertically from the target, for example giving more view of the sceneray and less ground.")]
+	public Vector3 centerOffset = Vector3.zero;
+
+	[Tooltip("Set this as false if a component of a prefab being instanciated by Photon Network, and manually call OnStartFollowing() when and if needed.")]
+	public bool followOnStart = false;
+
+	// cached transform of the target
+	Transform cameraTransform;
+
+	// maintain a flag internally to reconnect if target is lost or camera is switched
+	bool isFollowing;
+
+	// Represents the current velocity, this value is modified by SmoothDamp() every time you call it.
+	private float heightVelocity = 0.0f;
+
+	// Represents the position we are trying to reach using SmoothDamp()
+	private float targetHeight = 100000.0f;
+
+	// Use this for initialization
+	private void Start ()
+	{
+		// Start following the target if wanted.
+		if (followOnStart)
+		{
+			OnStartFollowing();
+		}
+	}
+
+	/// <summary>
+	/// MonoBehaviour method called after all Update functions have been called. This is useful to order script execution. For example a follow camera should always be implemented in LateUpdate because it tracks objects that might have moved inside Update.
+	/// </summary>
+	private void LateUpdate ()
+	{
+		// The transform target may not destroy on level load,
+		// so we need to cover corner cases where the Main Camera is different everytime we load a new scene, and reconnect when that happens
+		if (cameraTransform == null && isFollowing)
+		{
+			OnStartFollowing();
+		}
+
+		// only follow is explicitly declared
+		if (isFollowing)
+		{
+			Apply();
+		}
+	}
+
+	/// <summary>
+	/// Raises the start following event. 
+	/// Use this when you don't know at the time of editing what to follow, typically instances managed by the photon network.
+	/// </summary>
+	public void OnStartFollowing()
+	{
+		
+		cameraTransform = Camera.main.transform;
+	}
+}
---
+++ AnTutorialTest/Assets/Scripts/PlayerAnimatorManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class PlayerAnimatorManager : MonoBehaviour
+{
+	public float DirectionDampTime = 0.25f;
+
+	private Animator animator;
+
+	/// <summary>
+	/// MonoBehaviour method called on GameObject by Unity during initialization phase.
+	/// </summary>
+	void Start ()
+	{
+		animator = GetComponent<Animator>();
+		if (!animator)
+		{
+			Debug.LogError("PlayerAnimatorManager is Missing Animator Component", this);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		// failSafe is missing Animator component on GameObject
+		if (!animator)
+		{
+			return;
+		}
+
+		// deal with Jumping
+		AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+		// deal with Jumping
+		AnimatorStateInfo stateInfo = animator.GetCurrentAnimatorStateInfo(0);
+
+		// only allow jumping if we are running.
+		if (stateInfo.IsName("Base Layer.Run"))
+		{
+			// When using trigger parameter
+			if (Input.GetButtonDown("Fire2"))
+			{
+				animator.SetTrigger("Jump");
+			}
+		}
+
+		// deal with movement
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical");
+
+		// prevent negative Speed.
+		if (v < 0)
+		{
+			v = 0;
+		}
+
+		// set the Animator Parameters
+		animator.SetFloat("Speed", h * h + v * v);
+		animator.SetFloat("Direction", h, DirectionDampTime, Time.deltaTime);
+	}
+}
