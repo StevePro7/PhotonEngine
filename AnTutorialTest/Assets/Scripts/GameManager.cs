f9945a3e46ad6e68d428cbb040f0f2943b176342
StevePro7 <steveprozune@hotmail.com>
2018-01-31 21:08:43 +0000

    Update game manager

--- AnTutorialTest/Assets/Scripts/GameManager.cs
+++ AnTutorialTest/Assets/Scripts/GameManager.cs
@@ -7,9 +7,32 @@ namespace SteveProStudios.AnTutorialTest
 	{
 		public static GameManager Instance;
 
+		[Tooltip("The prefab to use for representing the player")]
+		public GameObject playerPrefab;
+
 		private void Start()
 		{
 			Instance = this;
+
+			// in case we started this demo with the wrong scene being active, simply load the menu scene
+			if (!PhotonNetwork.connected)
+			{
+				SceneManager.LoadScene(0);
+				return;
+			}
+
+			// #Tip Never assume public properties are properly filled up, always check and inform the developer of it.
+			if (playerPrefab == null)
+			{
+				Debug.LogError("<Color=Red><b>Missing</b></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'", this);
+			}
+			else
+			{
+				Debug.Log("We are Instantiating LocalPlayer from " + SceneManagerHelper.ActiveSceneName);
+
+				// we're in a room. spawn a character for the local player. it gets synced by PhotonNetwork.Instatiate
+				PhotonNetwork.Instantiate(playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+			}
 		}
 
 		/// <summary>
