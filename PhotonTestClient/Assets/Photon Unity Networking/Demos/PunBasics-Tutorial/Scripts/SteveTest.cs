634b4bded882667d6ab4d707d95604e33c770a29
StevePro7 <steveprozune@hotmail.com>
2018-03-05 16:06:55 +0000

    Add Photon test client

---
+++ PhotonTestClient/Assets/Photon Unity Networking/Demos/PunBasics-Tutorial/Scripts/SteveTest.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ExitGames.Demos.DemoAnimator
+{
+
+	public class SteveTest : MonoBehaviour
+	{
+		[Tooltip("The UI Loader Anime")]
+		public LoaderAnime loaderAnime;
+
+		private string resultText = "default";
+
+		void Start()
+		{
+		}
+
+		void Update()
+		{
+		}
+
+		private void OnGUI()
+		{
+			float xPos = 5.0f;
+			float yPos = 5.0f;
+
+			float width = Screen.width;
+			float height = Screen.height / 2;
+
+			GUI.skin.button.fontSize = 30;
+			GUI.skin.button.fontStyle = FontStyle.Bold;
+
+			if (GUI.Button(new Rect(xPos, yPos, width, height), "START"))
+			{
+				resultText = "adriana";
+				if (loaderAnime != null)
+				{
+					loaderAnime.StartLoaderAnimation();
+				}
+			}
+
+			if (GUI.Button(new Rect(xPos, yPos + height, width, height), "STOP"))
+			{
+				resultText = "stevepro";
+				if (loaderAnime != null)
+				{
+					loaderAnime.StopLoaderAnimation();
+				}
+			}
+
+			GUI.skin.label.fontSize = 50;
+			GUI.Label(new Rect(xPos, yPos, width, height), resultText);
+		}
+	}
+
+}
