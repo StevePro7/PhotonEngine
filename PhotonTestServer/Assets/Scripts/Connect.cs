6478c21bff34706b5afd4f45a39b81dcce03f42e
StevePro7 <steveprozune@hotmail.com>
2018-03-05 16:27:15 +0000

    Add scene and script

---
+++ PhotonTestServer/Assets/Scripts/Connect.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Connect : MonoBehaviour
+{
+	string _gameVersion = "1";
+
+	// Use this for initialization
+	void Start ()
+	{
+		PhotonNetwork.ConnectUsingSettings(_gameVersion);
+	}
+	
+	// Update is called once per frame
+	void OnGUI()
+	{
+		string label = PhotonNetwork.connectionStateDetailed.ToString();
+		GUILayout.Label(label);
+	}
+
+	void OnJoinedLobby()
+	{
+		Debug.Log("I have joined the lobby");
+	}
+}
